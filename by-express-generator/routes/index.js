46111309a14ffab396bebf5c281b7c758e5ad847
yuki1729 <yuki.o1729@gmail.com>
2016-12-09 23:19:00 +0900

    render-ejs の関係確認

--- by-express-generator/routes/index.js
+++ by-express-generator/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+  { title: 'Express',
+    task:'task'
+  }
+  //renderでテンプレートエンジンを指定、受け渡し数値をその中に記載
+);
 });
 
 module.exports = router;
